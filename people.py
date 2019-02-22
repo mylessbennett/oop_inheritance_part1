f75be1a49c2a4523394198f2ca92b5dcc032accf
Myles Bennett <myles.bennett@hotmail.com>
2019-02-22 15:03:55 -0500

    completed exercise

--- people.py
+++ people.py
@@ -4,15 +4,31 @@ class Person:
     def __init__(self, name):
         self.name = name
 
-class Student:
+    def greeting(self):
+        return "Hello my name is {}".format(self.name)
+
+
+class Student(Person):
     """ Made to learn """
 
     def learn(self):
         return "I get it!"
 
 
-class Instructor:
+class Instructor(Person):
     """ Made to teach """
 
     def teach(self):
         return "An object is an instance of a class."
+
+
+nadia = Instructor('Nadia')
+chris = Student('Chris')
+
+print(nadia.greeting())
+print(chris.greeting())
+
+print(nadia.teach())
+print(chris.learn())
+# chris.teach()<-- getting error message saying Student doesn't have teach
+# attrib because it is inheriting from Person which doesn't have teach method
