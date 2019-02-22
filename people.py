47919600dedff841b90c245209ccb167c4e64882
Myles Bennett <myles.bennett@hotmail.com>
2019-02-22 14:31:11 -0500

    added person, instructor, and student classes

---
+++ people.py
@@ -0,0 +1,18 @@
+class Person:
+    """ Creates a person """
+
+    def __init__(self, name):
+        self.name = name
+
+class Student:
+    """ Made to learn """
+
+    def learn(self):
+        return "I get it!"
+
+
+class Instructor:
+    """ Made to teach """
+
+    def teach(self):
+        return "An object is an instance of a class."
